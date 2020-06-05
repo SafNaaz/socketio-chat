f6cfad0e1a82abefcb794a0cd5ce46f33c133152
SAFNAS <iamsafnas@gmail.com>
2020-06-05 21:33:42 +0530

    joined proper room with qs cdn

--- server.js
+++ server.js
@@ -3,31 +3,47 @@ const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
 
+const formatMessage = require("./utils/messages");
+const { userJoin, getCurrentUser } = require("./utils/users");
+
 const app = express();
 const server = http.createServer(app);
 io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "SuNut Bot";
+
 //Run when client connects
 io.on("connection", (socket) => {
   //   console.log("New WebSocket Connection");
 
-  //Welcome to current user
-  socket.emit("message", "Welcome to SuNuT");
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-  //Broadcast when a user connects
-  socket.broadcast.emit("message", "A user has joined the chat");
+    socket.join(user.room);
 
-  //Broadcast when client disconnects
-  socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    //Welcome to current user
+    socket.emit("message", formatMessage(botName, "Welcome to SuNuT"));
+
+    //Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
   });
 
   //Listen to chatMessage
   socket.on("chatMessage", (msg) => {
     // console.log(msg);
-    io.emit("message", msg);
+    io.emit("message", formatMessage("USER", msg));
+  });
+
+  //Broadcast when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 });
 
