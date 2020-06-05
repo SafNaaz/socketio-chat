a025d7585b9ab539608c5dbdbd1ba549c600c181
SAFNAS <iamsafnas@gmail.com>
2020-06-05 19:47:39 +0530

    logging new connection

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
+//Run when client connects
+io.on("connection", (socket) => {
+  console.log("New WebSocket Connection");
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
