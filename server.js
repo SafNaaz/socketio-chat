a22bc4f298611baadb46ed6426e8ac6f8550997f
SAFNAS <iamsafnas@gmail.com>
2020-06-05 22:05:25 +0530

    porper room name and users list

--- server.js
+++ server.js
@@ -62,13 +62,13 @@ io.on("connection", (socket) => {
         "message",
         formatMessage(botName, `${user.username} has left the chat`)
       );
-    }
 
-    // send users and room info
-    io.to(user.room).emit("roomUsers", {
-      room: user.room,
-      users: getRoomUsers(user.room),
-    });
+      // send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
   });
 });
 
